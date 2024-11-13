89f2ed132d277169a1d73d9ac37e5fa83fe22a4f
cara <cara656513@gmail.com>
2024-11-13 14:59:57 +0900

    useState 이동

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,37 +1,8 @@
-import MOCK_DATA from "../data/data";
 import MyPocketmon from "../dex-components/MyPocketmon";
 import PocketmonList from "../dex-components/PocketmonList";
 import DexStyle from "../style/DexStyle";
-import { useState } from "react";
 
-const Dex = () => {
-  const [addedPokemons, setAddedPokemon] = useState([]);
-
-  //추가
-  const AddPokemon = (e) => {
-    e.stopPropagation();
-
-    let selectedOne = {
-      ...MOCK_DATA.find((data) => data.id === +e.target.id),
-    };
-
-    if (addedPokemons.length === 6) {
-      alert("포켓볼이 부족합니다.");
-    } else if (addedPokemons.map((data) => data.id).includes(+e.target.id)) {
-      alert("이미 보유한 포켓몬입니다.");
-    } else {
-      setAddedPokemon([...addedPokemons, selectedOne]);
-    }
-  };
-
-  const DeletePokemon = (e) => {
-    e.stopPropagation();
-
-    let deletedData = addedPokemons.filter((data) => data.id !== +e.target.id);
-
-    setAddedPokemon(deletedData);
-  };
-
+const Dex = ({ addedPokemons, DeletePokemon, AddPokemon }) => {
   return (
     <>
       <DexStyle
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,13 +2,51 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Dex from "../pages/Dex";
 import Detail from "../pages/Detail";
+import { useState } from "react";
+import MOCK_DATA from "../data/data";
 
 const Router = () => {
+  const [addedPokemons, setAddedPokemon] = useState([]);
+
+  //추가
+  const AddPokemon = (e) => {
+    e.stopPropagation();
+
+    let selectedOne = {
+      ...MOCK_DATA.find((data) => data.id === +e.target.id),
+    };
+
+    if (addedPokemons.length === 6) {
+      alert("포켓볼이 부족합니다.");
+    } else if (addedPokemons.map((data) => data.id).includes(+e.target.id)) {
+      alert("이미 보유한 포켓몬입니다.");
+    } else {
+      setAddedPokemon([...addedPokemons, selectedOne]);
+    }
+  };
+
+  const DeletePokemon = (e) => {
+    e.stopPropagation();
+
+    let deletedData = addedPokemons.filter((data) => data.id !== +e.target.id);
+
+    setAddedPokemon(deletedData);
+  };
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dex" element={<Dex />} />
+        <Route
+          path="/dex"
+          element={
+            <Dex
+              addedPokemons={addedPokemons}
+              DeletePokemon={DeletePokemon}
+              AddPokemon={AddPokemon}
+            />
+          }
+        />
         <Route path="/detail/:id" element={<Detail />} />
       </Routes>
     </BrowserRouter>
